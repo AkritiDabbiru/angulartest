eb8dfbd2da2257049344eaf832c0eb1d668f06b1
AkritiDabbiru <122767397+AkritiDabbiru@users.noreply.github.com>
2023-02-02 10:59:35 +0530

    Add files via upload

---
+++ test2.sql
@@ -0,0 +1,8 @@
+create table info
+(Empcode varchar(20),
+name varchar(30),Designation varchar(40), email varchar(68),location varchar(28));
+insert into info values
+('1','Raj','manager','a@gmail.com','pune'),
+('2','Neha','developer','b@gmail.com','mumbai'),
+('3','Amit','hr','c2gmail.com','bglr');
+select * from info;
